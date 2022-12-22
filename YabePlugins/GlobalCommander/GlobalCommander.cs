73d38b70f3d5611d65062df11b79969f1bf368ea
fchaxel <fchaxel@8324893d-a7a8-4806-89d4-2f8fea5c7913>
2022-12-22 04:15:45 +0000

    mistake correction
    git-svn-id: https://svn.code.sf.net/p/yetanotherbacnetexplorer/code/trunk@398 8324893d-a7a8-4806-89d4-2f8fea5c7913

--- YabePlugins/GlobalCommander/GlobalCommander.cs
+++ YabePlugins/GlobalCommander/GlobalCommander.cs
@@ -43,30 +43,6 @@ namespace GlobalCommander
             Cursor.Current = Cursors.WaitCursor;
             this._yabeFrm = yabeFrm;
 
-            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
-            {
-
-                Assembly thisAssembly = Assembly.GetExecutingAssembly();
-
-                //Get the Name of the AssemblyFile
-                var name = args.Name.Substring(0, args.Name.IndexOf(',')) + ".dll";
-
-                //Load form Embedded Resources - This Function is not called if the Assembly is in the Application Folder
-                var resources = thisAssembly.GetManifestResourceNames().Where(s => s.EndsWith(name));
-                if (resources.Count() > 0)
-                {
-                    var resourceName = resources.First();
-                    using (Stream stream = thisAssembly.GetManifestResourceStream(resourceName))
-                    {
-                        if (stream == null) return null;
-                        var block = new byte[stream.Length];
-                        stream.Read(block, 0, block.Length);
-                        return Assembly.Load(block);
-                    }
-                }
-                return null;
-            };
-
             Icon = yabeFrm.Icon; // gets Yabe Icon
             InitializeComponent();
             
--- YabePlugins/GlobalCommander/Plugin.cs
+++ YabePlugins/GlobalCommander/Plugin.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
 using Yabe;
@@ -15,6 +17,30 @@ namespace GlobalCommander
         public void Init(YabeMainDialog yabeFrm) // This is the unique mandatory method for a Yabe plugin 
         {
             this._yabeFrm = yabeFrm;
+
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+            {
+
+                Assembly thisAssembly = Assembly.GetExecutingAssembly();
+
+                //Get the Name of the AssemblyFile
+                var name = args.Name.Substring(0, args.Name.IndexOf(',')) + ".dll";
+
+                //Load form Embedded Resources - This Function is not called if the Assembly is in the Application Folder
+                var resources = thisAssembly.GetManifestResourceNames().Where(s => s.EndsWith(name));
+                if (resources.Count() > 0)
+                {
+                    var resourceName = resources.First();
+                    using (Stream stream = thisAssembly.GetManifestResourceStream(resourceName))
+                    {
+                        if (stream == null) return null;
+                        var block = new byte[stream.Length];
+                        stream.Read(block, 0, block.Length);
+                        return Assembly.Load(block);
+                    }
+                }
+                return null;
+            };
 
             // Creates the Menu Item
             ToolStripMenuItem MenuItem = new ToolStripMenuItem();
