429cf652b81c2a17cc07ff826498c3b44bb04fa9
fchaxel <fchaxel@8324893d-a7a8-4806-89d4-2f8fea5c7913>
2023-01-01 14:37:05 +0000

    git-svn-id: https://svn.code.sf.net/p/yetanotherbacnetexplorer/code/trunk@412 8324893d-a7a8-4806-89d4-2f8fea5c7913

--- CodeExamples/BACnetSC_Hub/PKI/CertViewer/Form1.Designer.cs
+++ CodeExamples/BACnetSC_Hub/PKI/CertViewer/Form1.Designer.cs
@@ -30,23 +30,32 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.lblCopyright = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
             this.lblError = new System.Windows.Forms.Label();
             this.tmrError = new System.Windows.Forms.Timer(this.components);
             this.certTree = new System.Windows.Forms.TreeView();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripGetUri = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtPasswd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // lblCopyright
+            // lblInfo
             // 
-            this.lblCopyright.AutoSize = true;
-            this.lblCopyright.Location = new System.Drawing.Point(16, 9);
-            this.lblCopyright.Name = "lblCopyright";
-            this.lblCopyright.Size = new System.Drawing.Size(165, 13);
-            this.lblCopyright.TabIndex = 0;
-            this.lblCopyright.Text = "Drop a Certificate File to Display it";
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(16, 9);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(165, 13);
+            this.lblInfo.TabIndex = 0;
+            this.lblInfo.Text = "Drop a Certificate File to Display it";
             // 
             // lblError
             // 
@@ -54,7 +63,7 @@
             this.lblError.BackColor = System.Drawing.SystemColors.Control;
             this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblError.ForeColor = System.Drawing.Color.Red;
-            this.lblError.Location = new System.Drawing.Point(42, 184);
+            this.lblError.Location = new System.Drawing.Point(42, 218);
             this.lblError.Name = "lblError";
             this.lblError.Size = new System.Drawing.Size(106, 13);
             this.lblError.TabIndex = 1;
@@ -72,14 +81,14 @@
             this.certTree.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.certTree.ImageIndex = 0;
             this.certTree.ImageList = this.imageList1;
-            this.certTree.Location = new System.Drawing.Point(11, 155);
+            this.certTree.Location = new System.Drawing.Point(11, 189);
             this.certTree.Name = "certTree";
             this.certTree.SelectedImageIndex = 0;
             this.certTree.ShowPlusMinus = false;
-            this.certTree.Size = new System.Drawing.Size(170, 76);
+            this.certTree.Size = new System.Drawing.Size(170, 109);
             this.certTree.TabIndex = 2;
             this.certTree.BeforeCollapse += new System.Windows.Forms.TreeViewCancelEventHandler(this.certTree_BeforeCollapse);
-            this.certTree.MouseUp += new System.Windows.Forms.MouseEventHandler(this.certTree_MouseUp);
+            this.certTree.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.certTree_AfterSelect);
             // 
             // imageList1
             // 
@@ -90,12 +99,81 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(33, 25);
+            this.pictureBox1.Location = new System.Drawing.Point(32, 52);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(128, 128);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.richTextBox1.Location = new System.Drawing.Point(187, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(538, 415);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripGetUri,
+            this.toolStripAbout});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(108, 48);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // toolStripGetUri
+            // 
+            this.toolStripGetUri.Name = "toolStripGetUri";
+            this.toolStripGetUri.Size = new System.Drawing.Size(107, 22);
+            this.toolStripGetUri.Text = "Paste";
+            this.toolStripGetUri.Click += new System.EventHandler(this.toolStripGetUri_Click);
+            // 
+            // toolStripAbout
+            // 
+            this.toolStripAbout.Name = "toolStripAbout";
+            this.toolStripAbout.Size = new System.Drawing.Size(107, 22);
+            this.toolStripAbout.Text = "About";
+            this.toolStripAbout.Click += new System.EventHandler(this.toolStripAbout_Click);
+            // 
+            // txtPasswd
+            // 
+            this.txtPasswd.Location = new System.Drawing.Point(12, 405);
+            this.txtPasswd.Name = "txtPasswd";
+            this.txtPasswd.PasswordChar = '*';
+            this.txtPasswd.Size = new System.Drawing.Size(165, 20);
+            this.txtPasswd.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 389);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Optional password";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 22);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Open with .NET viewer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "or Paste an URL using the Menu";
             // 
             // Form1
             // 
@@ -104,11 +182,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(193, 239);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(741, 439);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPasswd);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.lblError);
-            this.Controls.Add(this.lblCopyright);
+            this.Controls.Add(this.lblInfo);
             this.Controls.Add(this.certTree);
+            this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -117,6 +201,7 @@
             this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
             this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -124,12 +209,20 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblCopyright;
+        private System.Windows.Forms.Label lblInfo;
         private System.Windows.Forms.Label lblError;
         private System.Windows.Forms.Timer tmrError;
         private System.Windows.Forms.TreeView certTree;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox txtPasswd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripGetUri;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripAbout;
     }
 }
 
